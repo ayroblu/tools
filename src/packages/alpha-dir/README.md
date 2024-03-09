287764df7eb613bf087f40bca0b80b7e2e15086f
Ben Lu <ayroblu@gmail.com>
2024-03-09 09:44:30 +0000

    working

--- src/packages/alpha-dir/README.md
+++ src/packages/alpha-dir/README.md
@@ -2,3 +2,8 @@ Alpha-dir
 =========
 
 This converts a directory into a blob of alpha (a-z) characters (with spaces) that is useful for inputting into a keyboard analyzer (https://patorjk.com/keyboard-layout-analyzer/#/main)
+
+```sh
+cd [this-dir]
+npm link
+```
--- src/packages/alpha-dir/index.ts
+++ src/packages/alpha-dir/index.ts
@@ -1,5 +1,6 @@
 import { getArgs } from "./args";
 import fs from "fs";
+import path from "path";
 
 (() => {
   const { files } = getArgs();
@@ -10,8 +11,24 @@ import fs from "fs";
   console.log(allText.join("\n"));
 })();
 
-function getFilepaths(paths: string[]): string[] {
-  return paths.flatMap((path) =>
-    fs.lstatSync(path).isDirectory() ? getFilepaths([path]) : path,
+function getFilepaths(filepaths: string[]): string[] {
+  return filepaths.flatMap((filepath) =>
+    tryFn(
+      () => fs.lstatSync(filepath).isDirectory(),
+      () => console.log("filepath", filepath),
+    )
+      ? getFilepaths(
+          fs.readdirSync(filepath).map((p) => path.join(filepath, p)),
+        )
+      : filepath,
   );
+}
+
+function tryFn<T>(f: () => T, c: () => void): T {
+  try {
+    return f();
+  } catch (err) {
+    c();
+    throw err;
+  }
 }
